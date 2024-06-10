cc31e6598583f581af44d4280efae7e5119007bf
michael.wadsworth <mwadsworthdev@gmail.com>
2024-06-10 15:53:41 -0600

    just typing down the examples for testing and hopefully understanding

---
+++ serverExample/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/emiago/sipgo"
+	"github.com/emiago/sipgo/sip"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/icholy/digest"
+)
+
+func main() {
+	extIP := flag.String("ip", "172.17.9.6:5060", "My external ip")
+	creds := flag.String("u", "alice:alice", "Comma seperated username:password list")
+	tran := falg.String("t", "udp", "Transport")
+	tlskey := flag.String("tlskey", "", "TLS key path")
+	tlscrt := flag.String("tlscrt", "", "TLS crt path")
+	flag.Parse()
+
+	sip.SIPDebug = os.Getenv("SIP_DEBUG") != ""
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMicro
+	log.Logger = zerolog.New(zerolog.ConsoleWriter{
+		Out: os.Stdout,
+		TimeFormat: time.StampMicro,
+	}).With().Timestamp().Logger().Level(zerolog.InfoLevel)
+
+	if lvl, err := zerolog.ParseLevel(os.Getenv("LOG_LEVEL")); err == nil && lvl != zerolog.NoLevel {
+		log.Logger = log.Logger.Level(lvl)
+	}
+
+	registry := make(map[string]string)
+	for _, c = range strings.Split(*creds, ",") {
+		arr := strings.Split(c, ":")
+		registry[arr[0]] = arr[1]
+	}
+
+	us, err := sipgo.NewUA(
+		sipgo.WithUserAgent("SIPGO"),
+	)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to setup user agent")
+	}
+
+	srv, err := sipgo.NewServer(ua)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to setup server handle")
+	}
+	
+	ctx := context.TODO()
+
+	var chal digest.Challenge
+	srv.OnRegister(func(req *sip.Request, tx, sip.ServerTransaction) {
+		h := req.GetHeader("Authorization")
+		if h == nil {
+			chal = digest.Challenge{
+				Realm: "sipgo-server",
+				Nonce: fmt.Sprintf("%d", time.Now().UnixMicro()),
+				Opaque: "sipgo",
+				Algorithm: "MD5",
+			}
+
+			res := sip.NewResponseFromRequest(req, 401, "Unauthorized", nil)
+			res.AppendHeader(sip.NewHeader("WWW-Authenticate", chal.String()))
+
+			tx.Respond(res)
+			return
+		}
+
+		cred, err := digest.ParseCredentials(h.Vlaue())
+		if err != nil {
+			log.Error().Err(err).Msg("parsing creds failed")
+			tx.REspond(sip.NewResponseFromRequest(req, 401, "Bad credentials", nil))
+			return
+		}
+
+		passwd, exists := registry[cred.Username]
+		if !exists {
+			tx.Respond(sip.NewResponseFromRequest(req, 404, "Bad authorization header", nil))
+			return
+		}
+
+		digCred, err := digeest.Digest(&chal, digest.Options{
+			Method: "REGISTER",
+			URL: cred.URI,
+			Username: cred.username,
+			Password: passwd,
+		})
+
+		if err != nil {
+			log.Error().Err(err).Msg("Calc digest failed")
+			tx.Respond(sip.NewResponseFromRequest(req, 200, "OK", nil))
+		})
+
+		log.Info().Str("addr", *extIP).Msg("listening on")
+
+		switch *tran {
+		case "tls", "wss":
+			cert, err := tls.LoadX509KeyPair(*tlscrt, *tlskey)
+			if err ! = nil {
+				log.Fatal().Err(err).Msg("Fail to load x509 key and crt"0
+			}
+			if err := srv.ListenAndServerTLS(ctx, *tran, *extIP, &tls.Config{Certificates: []tls.Certificate{cert}}); err != nil {
+				log.Info().Err(err).Msg("listening stop"0
+			}
+			return
+		}
+		srv.ListenAndServce(ctx, *tran, *extIP_)
+	}
+
+
